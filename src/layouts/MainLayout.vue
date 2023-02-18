7efd89aede077e7840c5ff80cd91620ef11690e0
maguette-fall-seye <maguettefseye@gmail.com>
2023-02-18 18:26:05 +0000

    first commit

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,20 +1,20 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar class="bg-green-8 text-white shadow-2 ">
-      <q-btn flat label="Homepage" />
-      <q-space />
+      <q-toolbar class="bg-green-8 text-white shadow-2">
+        <q-btn flat label="Homepage" />
+        <q-space />
 
-      <!--
+        <!--
         notice shrink property since we are placing it
         as child of QToolbar
       -->
-      <q-tabs v-model="tab" shrink>
-        <q-tab name="tab1" label="accueil" />
-        <q-tab name="tab2" label="apropos" />
-        <q-tab name="tab3" label="contact" />
-      </q-tabs>
-    </q-toolbar>
+        <q-tabs v-model="tab" shrink>
+          <q-tab name="tab1" label="accueil" />
+          <q-tab name="tab2" label="apropos" />
+          <q-tab name="tab3" label="contact" />
+        </q-tabs>
+      </q-toolbar>
     </q-header>
 
     <!-- <q-drawer
@@ -43,76 +43,73 @@
   </q-layout>
 </template>
 
-
-
 <script>
-
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
   },
   {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
   },
   {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
   },
   {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
   // components: {
   //   EssentialLink
   // },
 
-  setup () {
-    const leftDrawerOpen = ref(false)
-   
+  setup() {
+    const leftDrawerOpen = ref(false);
+
     return {
       essentialLinks: linksList,
       leftDrawerOpen,
-      tab: ref(''),
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+      tab: ref(""),
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,27 +1,25 @@
 <template>
-   <q-card class="my-card" flat >
-      <q-card-section horizontal>
-        <q-card-section class="q-pt-xs">
-          <div class="text-overline">Agriculture</div>
-          <div class="text-h5 q-mt-sm q-mb-xs">Vente de périmètre</div>
-          <div class="text-caption text-grey">
-            Vente de périmètre pour ceux qui veulent investir dans le domaine de l'agriculture comme dans le domaine de l'aviculture.
-            Dans une zone très accessible à moins de 10 ms de la RN ou de l'autoroute Ila Touba
-          </div>
-        </q-card-section>
-
-        <q-card-section class="col-3 flex   flex-center">
-          <q-img
-            class="rounded-borders"
-            src="../assets/modou.png"
-          />
-        </q-card-section>
+  <q-card class="my-card" flat>
+    <q-card-section horizontal>
+      <q-card-section class="q-pt-xs">
+        <div class="text-overline">Agriculture</div>
+        <div class="text-h5 q-mt-sm q-mb-xs">Vente de périmètre</div>
+        <div class="text-caption text-grey">
+          Vente de périmètre pour ceux qui veulent investir dans le domaine de
+          l'agriculture comme dans le domaine de l'aviculture. Dans une zone
+          très accessible à moins de 10 ms de la RN ou de l'autoroute Ila Touba
+        </div>
       </q-card-section>
-   </q-card>
-   <div class="q-pa-md q-gutter-sm">
-    <q-banner  class="bg-green-8 text-white ">
 
-     Vente de terrain disponible. les terrains  sont dans une zone très accessible à moins de 10 ms de la RN ou de l'autoroute Ila Touba.
+      <q-card-section class="col-3 flex flex-center">
+        <q-img class="rounded-borders" src="../assets/modou.png" />
+      </q-card-section>
+    </q-card-section>
+  </q-card>
+  <div class="q-pa-md q-gutter-sm">
+    <q-banner class="bg-green-8 text-white">
+      Vente de terrain disponible. les terrains sont dans une zone très
+      accessible à moins de 10 ms de la RN ou de l'autoroute Ila Touba.
 
       <template v-slot:action>
         <!-- <q-btn flat color="white" label="Continue as a Guest" /> -->
@@ -29,112 +27,86 @@
       </template>
     </q-banner>
   </div>
-   <div class="q-pa-md row items-start q-gutter-md">
+  <div class="q-pa-md row items-start q-gutter-md">
     <q-card class="my-carde">
-      <img src="../assets/terrain.png">
+      <img src="../assets/terrain.png" />
 
       <q-card-section>
         <div class="text-h6">Our Changing Planet</div>
         <div class="text-subtitle2">by John Doe</div>
       </q-card-section>
 
-      <q-card-section class="q-pt-none">
-       dfghjured
-      </q-card-section>
+      <q-card-section class="q-pt-none"> dfghjured </q-card-section>
     </q-card>
     <q-card class="my-carde">
-      <img src="../assets/terrain1.png">
+      <img src="../assets/terrain1.png" />
 
       <q-card-section>
         <div class="text-h6">Our Changing Planet</div>
         <div class="text-subtitle2">by John Doe</div>
       </q-card-section>
 
-      <q-card-section class="q-pt-none">
-       dfghjured
-      </q-card-section>
+      <q-card-section class="q-pt-none"> dfghjured </q-card-section>
     </q-card>
     <q-card class="my-carde">
-      <img src="../assets/terrain2.png">
+      <img src="../assets/terrain2.png" />
 
       <q-card-section>
         <div class="text-h6">Our Changing Planet</div>
         <div class="text-subtitle2">by John Doe</div>
       </q-card-section>
 
-      <q-card-section class="q-pt-none">
-       dfghjured
-      </q-card-section>
+      <q-card-section class="q-pt-none"> dfghjured </q-card-section>
     </q-card>
     <q-card class="my-carde">
-      <img src="../assets/terrain3.png">
+      <img src="../assets/terrain3.png" />
 
       <q-card-section>
         <div class="text-h6">Our Changing Planet</div>
         <div class="text-subtitle2">by John Doe</div>
       </q-card-section>
 
-      <q-card-section class="q-pt-none">
-       dfghjured
-      </q-card-section>
+      <q-card-section class="q-pt-none"> dfghjured </q-card-section>
     </q-card>
     <q-card class="my-carde">
-      <img src="../assets/terrain4.png">
+      <img src="../assets/terrain4.png" />
 
       <q-card-section>
         <div class="text-h6">Our Changing Planet</div>
         <div class="text-subtitle2">by John Doe</div>
       </q-card-section>
 
-      <q-card-section class="q-pt-none">
-       dfghjured
-      </q-card-section>
+      <q-card-section class="q-pt-none"> dfghjured </q-card-section>
     </q-card>
     <q-card class="my-carde">
-      <img src="../assets/terrain6.png">
+      <img src="../assets/terrain6.png" />
 
       <q-card-section>
         <div class="text-h6">Our Changing Planet</div>
         <div class="text-subtitle2">by John Doe</div>
       </q-card-section>
 
-      <q-card-section class="q-pt-none">
-       dfghjured
-      </q-card-section>
+      <q-card-section class="q-pt-none"> dfghjured </q-card-section>
     </q-card>
-  
-  
-
-  
-
- 
-
-
- 
-
-  
-
-   
   </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
 <style lang="sass" scoped>
 .my-carde
   width: 100%
   max-width: 350px
   margin-bottom: 20px
-
 </style>
 <style scoped>
- div{
+div {
   margin-bottom: 5px;
-  margin: auto
- }
+  margin: auto;
+}
 </style>
